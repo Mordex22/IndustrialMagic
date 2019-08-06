417e409755982d4d20742afb633844bc6366f727
Mordex22 <mmberry96@gmail.com>
2019-08-06 00:25:16 -0500

    Creative Tab change
    Added another method to creative tab class

--- src/main/java/imagic/CreativeTabIndustrialMagic.java
+++ src/main/java/imagic/CreativeTabIndustrialMagic.java
@@ -5,15 +5,17 @@ import imagic.common.IndustrialMagicItems;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
 
-import javax.annotation.Nonnull;
-
 public class CreativeTabIndustrialMagic extends CreativeTabs {
 
     public CreativeTabIndustrialMagic() {
         super("tabIndustrialMagic");
     }
 
-    @Nonnull
+    @Override
+    public ItemStack getIconItemStack() {
+        return new ItemStack(IndustrialMagicItems.Ingot);
+    }
+
     @Override
     public ItemStack getTabIconItem() {
         return new ItemStack(IndustrialMagicItems.Ingot);
